e35c41d79e7cf00b7cd377796e186df54ea7544a
Fisher2911 <fisher1205l@gmail.com>
2022-12-04 00:10:08 -0500

    Add day 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import day.Day;
-import day.day3.Day3;
+import day.day4.Day4;
 
 public class Main {
 
     public static void main(String[] args) {
-        final Day day = new Day3();
+        final Day day = new Day4();
         day.part1();
         day.part2();
     }
---
+++ src/main/java/day/day4/Day4.java
@@ -0,0 +1,54 @@
+package day.day4;
+
+import day.Day;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Day4 extends Day {
+
+    public Day4() {
+        super("day4.txt");
+    }
+
+    @Override
+    public void part1() {
+        System.out.println(
+                this.readLines()
+                .map(s -> Map.entry(genRange(s.split(",")[0]), genRange(s.split(",")[1])))
+                .map(e -> e.getKey().containsAll(e.getValue()) || e.getValue().containsAll(e.getKey()))
+                .mapToInt(b -> b ? 1 : 0)
+                        .sum()
+        );
+    }
+
+    private Set<Integer> genRange(String s) {
+        final var set = new HashSet<Integer>(0);
+        for (int i = Integer.parseInt(s.split("-")[0]); i <= Integer.parseInt(s.split("-")[1]); i++) {
+            set.add(i);
+        }
+        return set;
+    }
+
+    @Override
+    public void part2() {
+        System.out.println(
+                this.readLines()
+                        .map(s -> Map.entry(genRange(s.split(",")[0]), genRange(s.split(",")[1])))
+                        .map(e -> containsAny(e.getKey(), e.getValue()))
+                        .mapToInt(b -> b ? 1 : 0)
+                        .sum()
+        );
+    }
+
+    private boolean containsAny(Set<Integer> first, Set<Integer> second) {
+        for (int i : first) {
+            if (second.contains(i)) return true;
+        }
+        for (int i : second) {
+            if (first.contains(i)) return true;
+        }
+        return false;
+    }
+}
