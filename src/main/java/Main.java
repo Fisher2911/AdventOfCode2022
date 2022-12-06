6df235c95831696a5f2994df896f00f3952bfd39
Fisher2911 <fisher1205l@gmail.com>
2022-12-06 15:25:58 -0500

    Added day 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import day.Day;
-import day.day5.Day5;
+import day.day6.Day6;
 
 public class Main {
 
     public static void main(String[] args) {
-        final Day day = new Day5();
+        final Day day = new Day6();
         day.part1();
         day.part2();
     }
---
+++ src/main/java/day/day6/Day6.java
@@ -0,0 +1,60 @@
+package day.day6;
+
+import day.Day;
+
+import java.util.LinkedList;
+
+public class Day6 extends Day {
+
+    public Day6() {
+        super("day6.txt");
+    }
+
+    @Override
+    public void part1() {
+        final char[] array = this.readLines().findFirst().orElseThrow().toCharArray();
+        final LinkedList<Character> stack = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (stack.size() == 4) {
+                index = i;
+                break;
+            }
+            final char c = array[i];
+            stack.add(c);
+            final int duplicateIndex = stack.indexOf(c);
+            if (duplicateIndex != stack.size() - 1) {
+                int temp = 0;
+                while (temp <= duplicateIndex) {
+                    stack.pop();
+                    temp++;
+                }
+            }
+        }
+        System.out.println(index);
+    }
+
+    @Override
+    public void part2() {
+        final char[] array = this.readLines().findFirst().orElseThrow().toCharArray();
+        final LinkedList<Character> stack = new LinkedList<>();
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (stack.size() == 14) {
+                index = i;
+                break;
+            }
+            final char c = array[i];
+            stack.add(c);
+            final int duplicateIndex = stack.indexOf(c);
+            if (duplicateIndex != stack.size() - 1) {
+                int temp = 0;
+                while (temp <= duplicateIndex) {
+                    stack.pop();
+                    temp++;
+                }
+            }
+        }
+        System.out.println(index);
+    }
+}
