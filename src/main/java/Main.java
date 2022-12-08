96671cc6d9066c24329ce261feff8f159de0ae2e
Fisher2911 <fisher1205l@gmail.com>
2022-12-08 16:15:24 -0500

    Added day 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import day.Day;
-import day.day7.Day7;
+import day.day8.Day8;
 
 public class Main {
 
     public static void main(String[] args) {
-        final Day day = new Day7();
+        final Day day = new Day8();
         day.part1();
         day.part2();
     }
