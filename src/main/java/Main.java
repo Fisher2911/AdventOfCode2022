d9837ffd3ea39b983862265a5a48780c19d038cc
Fisher2911 <fisher1205l@gmail.com>
2022-12-08 16:15:44 -0500

    Revert "Added day 7"
    This reverts commit 96671cc6d9066c24329ce261feff8f159de0ae2e.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import day.Day;
-import day.day8.Day8;
+import day.day7.Day7;
 
 public class Main {
 
     public static void main(String[] args) {
-        final Day day = new Day8();
+        final Day day = new Day7();
         day.part1();
         day.part2();
     }
